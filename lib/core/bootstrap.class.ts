244832359fbc5a059f4d7fefa6c641dfdd3a8557
Alexandre Moatty <heiso.android@gmail.com>
2016-10-06 16:26:59 +0200

    Fix type.

--- lib/core/bootstrap.class.ts
+++ lib/core/bootstrap.class.ts
@@ -1,6 +1,6 @@
 import {App, IApp} from './app.class'
 
-export function bootstrap(...BootstrapedApps: Array<IApp>): void {
+export function bootstrap(...BootstrapedApps: Array<any>): void {
   for (let BootstrapedApp of BootstrapedApps) {
     let app: IApp = App.apps.get(BootstrapedApp.name)
     app.start()
